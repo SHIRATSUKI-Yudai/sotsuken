c7cd910cb3619e3849e8eb3f00b1ed6f0b273fe6
SHIRATSUKI-Yudai <s2307@matsue-ct.ac.jp>
2024-09-29 17:52:16 +0900

    chmod

--- simu.sh
+++ simu.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-OMP_NUM_THREADS=8 mpiexec -n 16 ./ring_mono.out -o result/d_1024/snap -T 1000 -d 1024 -i result/d_1024/snap00155.dat --log_file result/d_1024/log2
+OMP_NUM_THREADS=8 mpiexec -n 16 ./ring_mono.out -o result/d_1024/snap -T 1000 -d 1024 -i result/d_1024/snap00276.dat --log_file result/d_1024/log2
